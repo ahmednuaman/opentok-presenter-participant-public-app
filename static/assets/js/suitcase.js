7856d39e2b26503adde213caa07bc61917286511
Ahmed Nuaman, Freelance Designer and Developer <ahmed@ahmednuaman.com>
2011-09-23 15:49:19 +0100

    added a nice loader

--- static/assets/js/suitcase.js
+++ static/assets/js/suitcase.js
@@ -1,4 +1,6 @@
 var S	= {
+	resizeFunctions												: [ ],
+	
 	ready														: function()
 	{
 		$( 'html' ).removeClass( 'no-js' );
@@ -12,7 +14,27 @@ var S	= {
 			$( '#container' ).remove();
 		}
 		
-		S.hideLoader();
+		S.setupLoader();
+		
+		$( window ).resize( S.windowResize ).resize();
+		
+		//S.hideLoader();
+	},
+	
+	windowResize												: function(e)
+	{
+		var win	= $( window );
+		var h	= win.height();
+		var w	= win.width();
+		var l	= S.resizeFunctions.length;
+		var f;
+		
+		for ( var i = 0; i < l; i++ )
+		{
+			f	= S.resizeFunctions[ i ];
+			
+			f( w, h );
+		}
 	},
 	
 	checkEventSource											: function()
@@ -20,7 +42,30 @@ var S	= {
 		return ( 'EventSource' in window );
 	},
 	
+	setupLoader													: function()
+	{
+		S.resizeFunctions.push( S.repositionLoader );
+	},
 	
+	repositionLoader											: function(w, h)
+	{
+		var l	= $( '#loader-inner' );
+		
+		l.css({
+			top		: ( h - l.height() ) / 2,
+			left	: ( w - l.width() ) / 2
+		});
+	},
+	
+	hideLoader													: function()
+	{
+		$( '#loader' ).stop( true ).fadeOut();
+	},
+	
+	showLoader													: function()
+	{
+		$( '#loader' ).stop( true ).fadeOut();
+	}
 };
 
 $( document ).ready( S.ready );
